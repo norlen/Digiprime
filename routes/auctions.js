1b93aa2f020df07d52841a98979911dac18ba0ec
Gustav <gustav.gurra@hotmail.com>
2022-01-27 09:02:18 +0100

    front-end för auktion, nuvarande och historik

--- routes/auctions.js
+++ routes/auctions.js
@@ -18,13 +18,6 @@ router
   .post(isLoggedIn, catchAsync(auction.createAuction))
 
 router
-	.route('/currentAuctions')
-	.get(catchAsync(auction.currentAuctions))
-
-
-router.get('/auction', auction.auction);
-
-router
   .route('/:id')
   .get(isLoggedIn, catchAsync(auction.show))
   .post(isLoggedIn, catchAsync(auction.placeBid))
